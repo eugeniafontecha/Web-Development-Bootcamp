44a8f9f44da53c877f37d8be950ef13c496019a6
eugeniafontecha <83308683+eugeniafontecha@users.noreply.github.com>
2022-01-18 13:30:22 -0300

    Functions - Parameters and Arguments

---
+++ JavaScript - Introduction/lifeInWeeks.js
@@ -0,0 +1,20 @@
+// Create a function that tells us how many days, weeks and months
+// we have left if we live until 90 years old.
+
+function lifeInWeeks(age) {
+    
+    /************Don't change the code above************/    
+        
+        //Write your code here.
+        var yearsLeft = 90 - age;
+        var days = yearsLeft*365;
+        var weeks = yearsLeft*52;
+        var months = yearsLeft*12;
+    
+        console.log("You have " + days + " days, " + weeks + " weeks, and " + months + " months left.");        
+        
+        
+    /*************Don't change the code below**********/
+    }
+    
+    
